6774595c095b5b81bef6c1d3dbbf1649dcf608ac
pumpkineggs <roman.saprykin@gmail.com>
2017-06-13 19:25:00 +0300

    Start wildcard calculation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 default_ip = (192, 168, 0, 1)
-prefix = 27
+prefix = 17
 default_mask_decimal = (255, 255, 255, 0)
 mask_full_octets = prefix // 8
 mask_remain_bits = prefix % 8
@@ -7,7 +7,7 @@ mask_binary = []
 mask_decimal = [0, 0, 0, 0]
 wildcard_decimal = [0, 0, 0, 0]
 print 'Default Mask: {:08b} {:08b} {:08b} {:08b}'.format(default_mask_decimal[0], default_mask_decimal[1],
-                                                 default_mask_decimal[2], default_mask_decimal[3])
+                                                         default_mask_decimal[2], default_mask_decimal[3])
 print 'Prefix: ', prefix
 print 'Full octets will first: ', mask_full_octets
 print 'Mask remain bits: ', mask_remain_bits
@@ -15,6 +15,8 @@ if mask_full_octets > 0:
     for i in range(mask_full_octets):
         mask_decimal[i] = 255
     print 'Mask first stage preparation: ', mask_decimal
+    if mask_full_octets == 4:
+        print "It's a single host mask: ", '.'.join(str(e) for e in mask_decimal)
     if mask_remain_bits > 0:
         remain_octet_index = mask_full_octets - 4
         print 'Remain octet index:', remain_octet_index
@@ -22,11 +24,16 @@ if mask_full_octets > 0:
         mask_remain_octet = int(mask_remain_octet_str, 2)
         print 'Remain octet: ', mask_remain_octet
         mask_decimal[remain_octet_index] = mask_remain_octet
-        wildcard_decimal[remain_octet_index] = mask_remain_octet
-        print 'New mask: ', mask_decimal
-        print 'Wildcard: ', wildcard_decimal
-        print '.'.join(str(e) for e in wildcard_decimal)
+        # wildcard calculation - it's inverted mask
+        wildcard_str=''
+        for bit in mask_remain_octet_str:
+            if bit == "1":
+                wildcard_str += "0"
+            else:
+                wildcard_str += "1"
+        print 'Wildcard binary', wildcard_str #wildcard_decimal[remain_octet_index] = mask_remain_octet
+        print 'New mask: ', '.'.join(str(e) for e in mask_decimal)
+        #print 'Wildcard: ', '.'.join(str(e) for e in wildcard_decimal)
     else:
         print 'No remain bits!'
-
-
+        print "New mask", '.'.join(str(e) for e in mask_decimal)
